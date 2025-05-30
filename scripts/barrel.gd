21f462bdcf34c6f642593ace2fc6840a70a78d92
Max <danielesquivelgarcia@gmail.com>
2025-05-30 08:17:01 +0200

    Feat: starting add audio

--- scripts/barrel.gd
+++ scripts/barrel.gd
@@ -3,14 +3,12 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var boomRadio: CollisionShape2D = $HurtBox/CollisionShape2D2
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-#const RED = preload("res://resources/red.tres")
 var stats : CharacterStats = preload("res://resources/red.tres")
 var coin: PackedScene = preload("res://scenes/coin.tscn")
 var hit_particles_scene = preload("res://scenes/HitParticles.tscn")
 @onready var hit_flash: AnimationPlayer = $hitFlash
 
 func _on_health_component_on_damage_taken(damage) -> void:
-	print("damege took barrel")
 	hit_flash.play("hit_flash")
 
 func _on_health_component_on_dead() -> void:
